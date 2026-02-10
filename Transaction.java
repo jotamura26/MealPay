f39e2e18b9ad3396a4ff4347c28433865f7af709
John Otamura <jotamura26@students.polytechnic.org>
2026-02-10 12:02:21 -0800

    Started Transaction file and Moved MealPay text file

---
+++ Transaction.java
@@ -0,0 +1,25 @@
+public class Transaction{
+   
+   
+    private double Amount;
+    private String Date;
+    
+    
+    public Transaction(String myDate, double myAmount){
+        Date = myDate;
+        Amount = myAmount;
+    }
+
+    public String getDate(){
+        return Date;
+    }
+
+    public double getAmount(){
+        return Amount;
+    }
+
+    public String toString(){
+        
+    }
+
+}
